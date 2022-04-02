8f565edf831fbf0690b789bcc47ca28d266f690a
Mastagoon <inzilagiway@gmail.com>
2022-04-02 14:43:41 +0300

    added a run script

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,21 @@ const { OAuth2 } = google.auth
 
 
 const main = async () => {
-    const oAuth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-    oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
-    const calender = google.calendar({ version: 'v3', auth: oAuth2Client })
-    const event = await calender.events.list({ calendarId: 'primary', timeMin: new Date().toISOString(), maxResults: 1, orderBy: 'startTime', singleEvents: true })
-    const nextEvent = event.data
-    const { start, end } = nextEvent.items![0]
-    notifier.notify({
-        title: "Next Event",
-        message: `${nextEvent!.items![0].summary}\n${dayjs(start?.dateTime).format("hh:mm A")} - ${dayjs(end?.dateTime).format("hh:mm A")}` ?? "No Event",
-        icon: path.join(__dirname, "..", "public", "assets", "icon.png"),
-    })
-    process.exit()
+    try {
+        const oAuth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET)
+        oAuth2Client.setCredentials({ refresh_token: process.env.ACCOUNT_REFRESH_TOKEN })
+        const calender = google.calendar({ version: 'v3', auth: oAuth2Client })
+        const event = await calender.events.list({ calendarId: 'primary', timeMin: new Date().toISOString(), maxResults: 1, orderBy: 'startTime', singleEvents: true })
+        const nextEvent = event.data
+        const { start, end } = nextEvent.items![0]
+        notifier.notify({
+            title: "Next Event",
+            message: `${nextEvent!.items![0].summary}\n${dayjs(start?.dateTime).format("hh:mm A")} - ${dayjs(end?.dateTime).format("hh:mm A")}` ?? "No Event",
+            icon: path.join(__dirname, "..", "public", "assets", "icon.png"),
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 main()
